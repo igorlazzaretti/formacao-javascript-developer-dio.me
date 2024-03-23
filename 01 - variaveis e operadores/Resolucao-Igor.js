34ce9066c05f93e8334693e8279565aff8e5a004
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2024-03-22 22:48:48 -0300

    commit

--- 01 - variaveis e operadores/Resolucao-Igor.js
+++ 01 - variaveis e operadores/Resolucao-Igor.js
@@ -16,3 +16,4 @@ const const_distanciaEmKm = 100;
 var var_LitrosConsumidos =  const_distanciaEmKm / const_KmPorLitros;
 var var_valorGasto = var_LitrosConsumidos * const_PrecoCombustivel;
 console.log(var_valorGasto);
+console.log(var_valorGasto.toFixed(2)); // INSERE 2 CASAS DECIMAIS
--- 02 - condicionais/aula.js
+++ 02 - condicionais/aula.js
@@ -1,5 +1,5 @@
 
-const numero = 5;
+const numero = 2;
 const ehNumeroPar = (numero % 2) === 0;
 
 if (ehNumeroPar) {
@@ -7,3 +7,24 @@ if (ehNumeroPar) {
 } else {
     console.log('O número é impar');
 }
+if (!ehNumeroPar){
+    console.log('onumeroehimpar')
+};
+
+
+// % RESTO DA DIVISAO 
+
+// === COMPARA EXATAMENTE
+// == IGNORA O TIPO DA VARIAVEL (NUMERO, STRING...)
+
+if(numero === 0){
+    console.log('Voce digitou numero inválido')
+}else if(ehNumeroPar){
+    console.log('Sim, este número é par.')
+} else {
+    console.log('Não, este número não é par, é impar')
+};
+
+
+// se variavel booleana coloco no nome da variavel is... 
+// exemplo isOddNumber inEvenNumber
